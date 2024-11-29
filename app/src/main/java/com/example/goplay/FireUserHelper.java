fe639fdb186592bb214e02ae048b6cb60835f505
Michael <mf260407@gmail.com>
2024-11-29 09:55:49 +0200

    edit profile start

---
+++ app/src/main/java/com/example/goplay/FireUserHelper.java
@@ -0,0 +1,86 @@
+package com.example.goplay;
+
+import android.util.Log;
+
+import com.example.goplay.database_classes.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import java.util.ArrayList;
+
+public class FireUserHelper {
+    private static final String TAG = "FireStoreHelper Tag";
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+    private static CollectionReference collectionRef = db.collection("users").document(currentUser.getUid()).collection("my_users");;
+    private FireUserHelper.FBReply fbReply;
+
+    public interface FBReply {
+        void getAllSuccess(ArrayList<User> users);
+        void getOneSuccess(User user);
+    }
+
+    public FireUserHelper(FireUserHelper.FBReply fbReply) {
+        this.fbReply = fbReply;
+    }
+
+    public void add(User user) {
+        collectionRef.add(user).addOnSuccessListener(documentReference -> {
+            Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+        }).addOnFailureListener(e -> {
+            Log.w(TAG, "Error adding document", e);
+            });
+    }
+
+    public void update(String id, User user) {
+        collectionRef.document(id).update("name", user.getName(), "sports", user.getSportsIntrest()).addOnSuccessListener(aVoid -> {
+            Log.d(TAG, "DocumentSnapshot updated with ID: " + id);
+        }).addOnFailureListener(e -> {
+            Log.w(TAG, "Error updating document", e);
+            });
+    }
+    public void delete(String id) {
+        collectionRef.document(id).delete().addOnSuccessListener(aVoid -> {
+            Log.d(TAG, "DocumentSnapshot deleted with ID: " + id);
+        }).addOnFailureListener(e -> {
+            Log.w(TAG, "Error deleting document", e);
+        });
+    }
+    public void getAll() {
+        collectionRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                ArrayList<User> users = new ArrayList<>();
+                for (com.google.firebase.firestore.DocumentSnapshot document : task.getResult()) {
+                    Log.d(TAG, document.getId() + " => " + document.getData());
+                    User user = document.toObject(User.class);
+                    users.add(user);
+                }
+                fbReply.getAllSuccess(users);
+            } else {
+                Log.w(TAG, "Error getting documents.", task.getException());
+            }
+        });
+
+    }
+    public void getOne(String id) {
+        collectionRef.document(id).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                com.google.firebase.firestore.DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    User user = document.toObject(User.class);
+                    fbReply.getOneSuccess(user);
+                } else {
+                    Log.d(TAG, "No such document");
+                }
+            } else {
+                Log.d(TAG, "get failed with ", task.getException());
+            }
+        });
+    }
+
+    public static CollectionReference getCollectionRef() {
+        return collectionRef;
+    }
+}
--- app/src/main/java/com/example/goplay/database_classes/User.java
+++ app/src/main/java/com/example/goplay/database_classes/User.java
@@ -3,8 +3,54 @@ package com.example.goplay.database_classes;
 public class User {
     private int user_id;
     private String name;
-    private String email;
-    private String password;
     private String sportsIntrest;
     private String location;
+
+
+    public User() {
+    }
+
+    public User(int user_id, String name, String sportsIntrest, String location) {
+        this.user_id = user_id;
+        this.name = name;
+        this.sportsIntrest = sportsIntrest;
+        this.location = location;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+
+    public String getSportsIntrest() {
+        return sportsIntrest;
+    }
+
+    public void setSportsIntrest(String sportsIntrest) {
+        this.sportsIntrest = sportsIntrest;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
+
